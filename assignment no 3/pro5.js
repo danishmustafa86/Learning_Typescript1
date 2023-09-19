6d6a575045ff4d66178f01f48f77509d5ac67aeb
Danish jajja <danishjajja86@gmail.com>
2023-09-19 22:55:58 +0500

    pro5 changing

--- assignment no 3/pro5.js
+++ assignment no 3/pro5.js
@@ -1,4 +1,4 @@
-// **5** - Develop a program that reads a list of grades and uses the splice method to remove failing grades (below 50) from the array.
+// **5** - Develop a program that reads a list of grades & uses the splice method to remove failing grades (below 50) from the array.
 function Passinggrades(gradesarry) {
     for (var index = gradesarry.length - 1; index >= 0; index--) {
         if (gradesarry[index] < 50) {
@@ -7,4 +7,4 @@ function Passinggrades(gradesarry) {
     }
     return gradesarry;
 }
-console.log(Passinggrades([43, 67, 58, 97, 19, 52, 34, 56, 34, 29, 13, 44, 23, 42, 8, 66, 23, 74, 39]));
+console.log(Passinggrades([43, 67, 58, 52, 34, 56, 34, 29, 13, 44, 23, 42, 8, 66, 23, 74, 39]));
