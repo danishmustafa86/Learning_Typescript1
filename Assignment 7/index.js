99146c6c156aefd621207b883f9881e8dfdc0925
Danish jajja <danishjajja86@gmail.com>
2023-09-26 22:09:24 +0500

    types practice

---
+++ Assignment 7/index.js
@@ -0,0 +1,95 @@
+"use strict";
+// const complexObject = {
+//     name: "John Doe",
+//     age: 30,
+//     isStudent: false,
+//     interests: ["programming", "music", "hiking"],
+//     address: {
+//       street: "123 Main St",
+//       city: "Exampleville",
+//       postalCode: "12345",
+//     },
+//     grades: {
+//       math: {
+//         midterm: 85,
+//         final: 92,
+//       },
+//       science: {
+//         midterm: 78,
+//         final: 88,
+//       },
+//     },
+//     contact: {
+//       email: "john.doe@example.com",
+//       phone: "+1234567890",
+//     },
+//     tuple: [1, "two", true],
+//     functionExample: function (x) {
+//       return x * 2;
+//     },
+//   };
+const complexObject = {
+    name: "John Doe",
+    age: 30,
+    isStudent: false,
+    interests: ["programming", "music", "hiking"],
+    address: {
+        street: "123 Main St",
+        city: "Exampleville",
+        postalCode: "12345",
+    },
+    grades: {
+        math: {
+            midterm: 85,
+            final: 92,
+        },
+        science: {
+            midterm: 78,
+            final: 88,
+        },
+    },
+    contact: {
+        email: "john.doe@example.com",
+        phone: "+1234567890",
+    },
+    tuple: [1, "two", true],
+    functionExample: function (x) {
+        return x * 2;
+    },
+};
+// assigning types;
+let names = "danish";
+let age = 20;
+let isStudent = true;
+let interests = ["programming", "music", "hiking"];
+let address = {
+    street: "123 Main St",
+    city: "Exampleville",
+    postalCode: "12345",
+};
+let grades = {
+    math: {
+        midterm: 85,
+        final: 92,
+    },
+    science: {
+        midterm: 78,
+        final: 88,
+    },
+};
+let contact = {
+    email: "john.doe@example.com",
+    phone: "+1234567890",
+};
+let tuple = [1, "two", true];
+let functionExample = function (x) {
+    return x * 2;
+};
+console.log(functionExample());
+console.log(age);
+console.log("yes I am a student", isStudent);
+console.log(tuple);
+console.log(contact);
+console.log(grades);
+console.log(Object);
+console.log(names);
---
+++ Assignment 7/index.ts
@@ -0,0 +1,106 @@
+// const complexObject = {
+//     name: "John Doe",
+//     age: 30,
+//     isStudent: false,
+//     interests: ["programming", "music", "hiking"],
+//     address: {
+//       street: "123 Main St",
+//       city: "Exampleville",
+//       postalCode: "12345",
+//     },
+    
+//     grades: {
+//       math: {
+//         midterm: 85,
+//         final: 92,
+//       },
+//       science: {
+//         midterm: 78,
+//         final: 88,
+//       },
+//     },
+//     contact: {
+//       email: "john.doe@example.com",
+//       phone: "+1234567890",
+//     },
+//     tuple: [1, "two", true],
+//     functionExample: function (x) {
+//       return x * 2;
+//     },
+//   };
+
+const complexObject = {
+  name: "John Doe",
+  age: 30,
+  isStudent: false,
+  interests: ["programming", "music", "hiking"],
+  address: {
+    street: "123 Main St",
+    city: "Exampleville",
+    postalCode: "12345",
+  },
+  
+  grades: {
+    math: {
+      midterm: 85,
+      final: 92,
+    },
+    science: {
+      midterm: 78,
+      final: 88,
+    },
+  },
+  contact: {
+    email: "john.doe@example.com",
+    phone: "+1234567890",
+  },
+  tuple: [1, "two", true],
+  functionExample: function (x:number) {
+    return x * 2;
+  },
+};
+// making types
+type addresss={street:string,city:string,postalCode:string}
+type contacts={email:string,phone:string}
+type maths={midterm:number,final:number}
+type sciences={midterm:number,final:number}
+type gradees={math:maths,science:sciences};
+// assigning types;
+
+let names: string="danish"
+let age: number=20
+let isStudent: boolean=true
+let interests: [string,string,string]=["programming", "music", "hiking"]
+let address: addresss={
+  street: "123 Main St",
+  city: "Exampleville",
+  postalCode: "12345",
+}
+let grades: gradees= {
+  math: {
+    midterm: 85,
+    final: 92,
+  },
+  science: {
+    midterm: 78,
+    final: 88,
+  },
+}
+let contact:contacts={
+  email: "john.doe@example.com",
+  phone: "+1234567890",
+}
+let tuple:[number,string,boolean]= [1, "two", true]
+let functionExample:any = function (x:number) {
+  return x * 2;
+}
+console.log(functionExample());
+console.log(age);
+console.log("yes I am a student",isStudent);
+console.log(tuple);
+console.log(contact);
+console.log(grades);
+console.log(names);
+
+
+
