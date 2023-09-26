8fab164972ceefd413bbf3af13be5ac60f3aaa7b
Danish jajja <danishjajja86@gmail.com>
2023-09-26 22:18:02 +0500

    types assignment

--- Assignment 7/index.js
+++ Assignment 7/index.js
@@ -1,33 +1,4 @@
 "use strict";
-// const complexObject = {
-//     name: "John Doe",
-//     age: 30,
-//     isStudent: false,
-//     interests: ["programming", "music", "hiking"],
-//     address: {
-//       street: "123 Main St",
-//       city: "Exampleville",
-//       postalCode: "12345",
-//     },
-//     grades: {
-//       math: {
-//         midterm: 85,
-//         final: 92,
-//       },
-//       science: {
-//         midterm: 78,
-//         final: 88,
-//       },
-//     },
-//     contact: {
-//       email: "john.doe@example.com",
-//       phone: "+1234567890",
-//     },
-//     tuple: [1, "two", true],
-//     functionExample: function (x) {
-//       return x * 2;
-//     },
-//   };
 const complexObject = {
     name: "John Doe",
     age: 30,
@@ -85,11 +56,10 @@ let tuple = [1, "two", true];
 let functionExample = function (x) {
     return x * 2;
 };
-console.log(functionExample());
-console.log(age);
+console.log("funtionExample is", functionExample());
+console.log("age is", age);
 console.log("yes I am a student", isStudent);
-console.log(tuple);
-console.log(contact);
-console.log(grades);
-console.log(Object);
-console.log(names);
+console.log("tuple is", tuple);
+console.log("Contact is", contact);
+console.log("grades is", grades);
+console.log("name is", names);
--- Assignment 7/index.ts
+++ Assignment 7/index.ts
@@ -1,34 +1,3 @@
-// const complexObject = {
-//     name: "John Doe",
-//     age: 30,
-//     isStudent: false,
-//     interests: ["programming", "music", "hiking"],
-//     address: {
-//       street: "123 Main St",
-//       city: "Exampleville",
-//       postalCode: "12345",
-//     },
-    
-//     grades: {
-//       math: {
-//         midterm: 85,
-//         final: 92,
-//       },
-//       science: {
-//         midterm: 78,
-//         final: 88,
-//       },
-//     },
-//     contact: {
-//       email: "john.doe@example.com",
-//       phone: "+1234567890",
-//     },
-//     tuple: [1, "two", true],
-//     functionExample: function (x) {
-//       return x * 2;
-//     },
-//   };
-
 const complexObject = {
   name: "John Doe",
   age: 30,
@@ -91,16 +60,16 @@ let contact:contacts={
   phone: "+1234567890",
 }
 let tuple:[number,string,boolean]= [1, "two", true]
-let functionExample:any = function (x:number) {
+let functionExample:any = function (x:any) {
   return x * 2;
 }
-console.log(functionExample());
-console.log(age);
+console.log("funtionExample is",functionExample());
+console.log("age is",age);
 console.log("yes I am a student",isStudent);
-console.log(tuple);
-console.log(contact);
-console.log(grades);
-console.log(names);
+console.log("tuple is",tuple);
+console.log("Contact is",contact);
+console.log("grades is",grades);
+console.log("name is",names);
 
 
 
