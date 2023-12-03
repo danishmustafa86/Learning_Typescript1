cff3bcd0fc5bcdf5ba85ddf4464b02044ff8860d
Danish jajja <danishjajja86@gmail.com>
2023-12-03 09:15:21 +0500

    oop class 2

--- OOPs class 2/encap.ts
+++ OOPs class 2/encap.ts
@@ -3,20 +3,20 @@
 //2 protected=only childs can use parents properties
 //3 privete=no one can use
 class bank{
-//    private balance:number=0
-//    protected balance:number=0
-   public balance:number=110
+   private balnce:number=890
+   public balance:number=50
+   public blance:number=110
     getBalance(){
-return this.balance
+return this.balnce
 }
 }
 let balan=new bank()
-// console.log("parent funtion is",balan.getBalance());
+console.log("parent funtion is",balan.getBalance());
 
 class account extends bank{
     public transactions:number[]=[1,2,3]
     public showbalance(){
-        return this.transactions
+        return this.blance
     }
     public trnsactions(){
         return this.trnsactions
@@ -26,6 +26,7 @@ var account1 =new account()
 console.log("koko",account1.balance);
 console.log("koko",account1.showbalance());
 console.log("kokob",account1.getBalance());
+console.log("kokob",account1.transactions);
 // console.log("koko",bank.get());
 
 
